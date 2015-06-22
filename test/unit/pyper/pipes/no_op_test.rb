ec215f8beaded99edac34653349be447d46de275
Brian Mosier <bmosier@gmail.com>
2015-06-22 17:59:51 -0400

    fixed the no_op test

--- test/unit/pyper/pipes/no_op_test.rb
+++ test/unit/pyper/pipes/no_op_test.rb
@@ -3,14 +3,9 @@ require_relative '../../../test_helper'
 module Pyper::Pipes
   class NoOpTest < Minitest::Should::TestCase
     context 'NoOp pipe' do
-
-      setup do
-        @pipe = { :pipe_var => "this is a string" }
-      end
-
       should 'perform no operation' do
-        Pyper::Pipes::NoOp
-        assert_equal({ :pipe_var => "this is a string" }, @pipe)
+        pipe_input = { :pipe_var => "this is a string" }
+        assert_equal pipe_input, NoOp.pipe(pipe_input)
       end
     end
   end
