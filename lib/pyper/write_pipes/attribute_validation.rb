84e395e3a148fa1870b45cfe69b8314171bf89fd
Anthony Mangano <anthony@backupify.com>
2015-07-06 16:47:07 -0400

    Adds whitelist validations

--- lib/pyper/write_pipes/attribute_validation.rb
+++ lib/pyper/write_pipes/attribute_validation.rb
@@ -7,8 +7,11 @@ module Pyper::WritePipes
     # An Array of presence validations to perform.
     attr_reader :presence_validations
 
-    # An Array of inclusion validations to perform.
+    # An Hash of inclusion validations to perform.
     attr_reader :inclusion_validations
+
+    # An Array of whitelisted attributes that may be set.
+    attr_reader :whitelist
 
     # @param opts [Hash] Options defining how attributes should be validated.
     # @option opts [Array<Symbol>] :presence Specifies a list of attributes for which
@@ -19,9 +22,13 @@ module Pyper::WritePipes
     #   attribute to validate and should point to an Array of valid values. If the
     #   actual value at that key is not included in the list of valid values,
     #   a Failure error will be raised.
+    # @option opts [Array<Symbol>] :whitelist If present, only allow the attributes
+    #   in the whitelist Array be set. If not present, allow all attributes to be
+    #   set.
     def initialize(opts={})
       @presence_validations = opts[:presence] || []
       @inclusion_validations = opts[:inclusion] || []
+      @whitelist = opts[:whitelist]
     end
 
     def pipe(attributes, status = {})
@@ -34,6 +41,14 @@ module Pyper::WritePipes
       inclusion_validations.each do |attr, valid_values|
         unless valid_values.include?(attributes[attr])
           raise Failure.new("Invalid value for attribute #{attr}.")
+        end
+      end
+
+      if whitelist.present?
+        attributes.keys.each do |attr|
+          unless whitelist.include?(attr)
+            raise Failure.new("Attribute #{attr} is not allowed.")
+          end
         end
       end
 
--- test/unit/pyper/write_pipes/attribute_validation_test.rb
+++ test/unit/pyper/write_pipes/attribute_validation_test.rb
@@ -3,46 +3,88 @@ require 'test_helper'
 module Pyper::WritePipes
   class AttributeValidationTest < Minitest::Should::TestCase
     context "pipe" do
-      setup do
-        @opts = {
-          :presence => [:foo, :bar],
-          :inclusion => { :bar => ["qux", "bazqux"] }
-        }
-        @pipe = AttributeValidation.new(@opts)
+      context "with presence validations" do
+        setup do
+          @opts = { :presence => [:foo, :bar] }
+          @pipe = AttributeValidation.new(@opts)
+        end
+
+        should "raise when missing required attribute" do
+          error_1 = assert_raises(AttributeValidation::Failure) do
+            @pipe.pipe({})
+          end
+          assert_equal "Missing required attribute foo.", error_1.message
+
+          error_2 = assert_raises(AttributeValidation::Failure) do
+            @pipe.pipe({ :foo => "bar" })
+          end
+          assert_equal "Missing required attribute bar.", error_2.message
+
+          # Nothing should be raised now.
+          @pipe.pipe({ :foo => "bar", :bar => "qux" })
+        end
+
+        should "raise when required attribute is nil" do
+          error = assert_raises(AttributeValidation::Failure) do
+            @pipe.pipe({ :foo => nil, :bar => "qux"})
+          end
+          assert_equal "Missing required attribute foo.", error.message
+        end
       end
 
-      should "raise when missing required attribute" do
-        error1 = assert_raises(AttributeValidation::Failure) do
-          @pipe.pipe({})
+      context "with inclusion validations" do
+        setup do
+          @opts = { :inclusion => { :bar => ["qux", "bazqux"] } }
+          @pipe = AttributeValidation.new(@opts)
         end
-        assert_equal "Missing required attribute foo.", error1.message
 
-        error2 = assert_raises(AttributeValidation::Failure) do
-          @pipe.pipe({ :foo => "bar" })
+        should "raise when attribute value is invalid" do
+          error = assert_raises(AttributeValidation::Failure) do
+            @pipe.pipe({ :bar => "foo"})
+          end
+          assert_equal "Invalid value for attribute bar.", error.message
+
+          # Nothing should be raised now.
+          @pipe.pipe({ :bar => "qux"})
+          @pipe.pipe({ :bar => "bazqux"})
         end
-        assert_equal "Missing required attribute bar.", error2.message
-
-        # Nothing should be raised now.
-        @pipe.pipe({ :foo => "bar", :bar => "qux" })
       end
 
-      should "raise when required attribute is nil" do
-        error1 = assert_raises(AttributeValidation::Failure) do
-          @pipe.pipe({ :foo => nil, :bar => "qux"})
+      context "whitelist validations" do
+        setup do
+          @opts = { :whitelist => [:foo, :bar] }
+          @pipe = AttributeValidation.new(@opts)
         end
-        assert_equal "Missing required attribute foo.", error1.message
-      end
 
-      should "raise when attribute value is invalid" do
-        error1 = assert_raises(AttributeValidation::Failure) do
-          @pipe.pipe({ :foo => "bar", :bar => "foo"})
+        should "raise when attribute is not included in the whitelist" do
+          error = assert_raises(AttributeValidation::Failure) do
+            @pipe.pipe({ :foo => "foo", :bar => "bar", :baz => "baz" })
+          end
+          assert_equal "Attribute baz is not allowed.", error.message
+
+          # Nothing should be raised now.
+          @pipe.pipe({ :foo => "foo", :bar => "bar" })
         end
-        assert_equal "Invalid value for attribute bar.", error1.message
+
+        should "allow all attributes when whitelist option is not set" do
+          pipe = AttributeValidation.new
+
+          # Nothing should be raised now.
+          pipe.pipe({ :foo => "foo", :bar => "bar", :baz => "baz" })
+        end
       end
 
       should "return attributes hash when all is well" do
-        attrs = { :foo => "bar", :bar => "bazqux" }
-        assert_equal attrs, @pipe.pipe(attrs)
+        opts = {
+          :whitelist => [:foo, :bar],
+          :presence => [:foo],
+          :inclusion => {
+            :bar => ["bar", "foobar"]
+          }
+        }
+        pipe = AttributeValidation.new(opts)
+        attributes = { :foo => "bar", :bar => "foobar" }
+        assert_equal attributes, pipe.pipe(attributes)
       end
     end
   end
