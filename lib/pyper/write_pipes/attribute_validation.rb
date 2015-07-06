e66bcf9ff625572a02ff5f6d14c53d6ac832f5ca
Anthony Mangano <anthony@backupify.com>
2015-07-06 12:14:18 -0400

    support both presence and inclusion validations

--- lib/pyper/write_pipes/attribute_validation.rb
+++ lib/pyper/write_pipes/attribute_validation.rb
@@ -1,27 +1,43 @@
 module Pyper::WritePipes
   class AttributeValidation
 
-    # Raised when a required attribute is missing.
-    class FailedValidation < ::StandardError; end
+    # Raised when validation fails.
+    class Failure < ::StandardError; end
 
-    # An Array of required attributes.
-    attr_reader :required_attributes
+    # An Array of presence validations to perform.
+    attr_reader :presence_validations
 
-    # @param required_attributes [Array] A list of required attributes to check for.
-    def initialize(required_attributes)
-      @required_attributes = required_attributes
+    # An Array of inclusion validations to perform.
+    attr_reader :inclusion_validations
 
-      unless @required_attributes.is_a?(Array)
-        raise ArgumentError.new("Expected required_arguments to be an Array. Got #{required_attributes.class}")
-      end
+    # @param opts [Hash] Options defining how attributes should be validated.
+    # @option opts [Array<Symbol>] :presence Specifies a list of attributes for which
+    #   presence validations should be performed. If any attribute in the provided list
+    #   is nil or missing, a FailedValidation error will be raised.
+    # @option opts [Array<Hash>] :inclusion Specifies a list of attributes for which
+    #   inclusion validations should be performed. Each Hash in the list should 
+    #   contain one key (the attribute) which points to an array of valid values.
+    #   If the actual value at that key is not included in the list of valid values,
+    #   a FailedValidation error will be raised.
+    def initialize(opts={})
+      @presence_validations = opts[:presence] || []
+      @inclusion_validations = opts[:inclusion] || []
     end
 
     def pipe(attributes, status = {})
-      required_attributes.each do |attr|
-        raise FailedValidation.new("Missing required attribute #{attr}.") if attributes[attr].nil?
+      presence_validations.each do |attr|
+        if attributes[attr].nil?
+          raise Failure.new("Missing required attribute #{attr}.")
+        end
       end
+
+      inclusion_validations.each do |attr, valid_values|
+        unless valid_values.include?(attributes[attr])
+          raise Failure.new("Invalid value for attribute #{attr}.")
+        end
+      end
+
       attributes
     end
-
   end
 end
--- test/unit/pyper/write_pipes/attribute_validation_test.rb
+++ test/unit/pyper/write_pipes/attribute_validation_test.rb
@@ -2,50 +2,48 @@ require 'test_helper'
 
 module Pyper::WritePipes
   class AttributeValidationTest < Minitest::Should::TestCase
-    context "initialize" do
-      should "raise when required_attributes is something other than an Array" do
-        error = assert_raises(ArgumentError) do
-          AttributeValidation.new({})
-        end
-
-        expected = "Expected required_arguments to be an Array. Got Hash"
-        assert_equal expected, error.message
-      end
-    end
-
     context "pipe" do
       setup do
-        @required_attrs = [:foo, :bar]
-        @pipe = AttributeValidation.new(@required_attrs)
+        @opts = {
+          :presence => [:foo, :bar],
+          :inclusion => { :bar => ["qux", "bazqux"] }
+        }
+        @pipe = AttributeValidation.new(@opts)
       end
 
       should "raise when missing required attribute" do
-        error1 = assert_raises(AttributeValidation::FailedValidation) do
+        error1 = assert_raises(AttributeValidation::Failure) do
           @pipe.pipe({})
         end
         assert_equal "Missing required attribute foo.", error1.message
 
-        error2 = assert_raises(AttributeValidation::FailedValidation) do
+        error2 = assert_raises(AttributeValidation::Failure) do
           @pipe.pipe({ :foo => "bar" })
         end
         assert_equal "Missing required attribute bar.", error2.message
 
         # Nothing should be raised now.
-        @pipe.pipe({ :foo => "bar", :bar => "foo" })
+        @pipe.pipe({ :foo => "bar", :bar => "qux" })
       end
 
       should "raise when required attribute is nil" do
-        error1 = assert_raises(AttributeValidation::FailedValidation) do
-          @pipe.pipe({ :foo => nil, :bar => "foo"})
+        error1 = assert_raises(AttributeValidation::Failure) do
+          @pipe.pipe({ :foo => nil, :bar => "qux"})
         end
         assert_equal "Missing required attribute foo.", error1.message
       end
 
+      should "raise when attribute value is invalid" do
+        error1 = assert_raises(AttributeValidation::Failure) do
+          @pipe.pipe({ :foo => "bar", :bar => "foo"})
+        end
+        assert_equal "Invalid value for attribute bar.", error1.message
+      end
+
       should "return attributes hash when all is well" do
-        attrs = { :foo => "bar", :bar => "foo" }
+        attrs = { :foo => "bar", :bar => "bazqux" }
         assert_equal attrs, @pipe.pipe(attrs)
       end
     end
-
   end
 end
