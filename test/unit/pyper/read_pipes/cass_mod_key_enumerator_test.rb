9696b7096c0eac62249f1b6d762dc45f86ba517a
Desmond Yeung <desmond@backupify.com>
2015-09-09 15:31:41 -0400

    create enough test data so it actually pages

--- test/unit/pyper/read_pipes/cass_mod_key_enumerator_test.rb
+++ test/unit/pyper/read_pipes/cass_mod_key_enumerator_test.rb
@@ -27,10 +27,14 @@ module Pyper::ReadPipes
       end
 
       should 'page through cassandra result and return all items' do
+        @client.insert(:mod_test, {row: 1, id: 'idd', mod_key: 0, val: '4'})
+        @client.insert(:mod_test, {row: 1, id: 'ide', mod_key: 1, val: '5'})
+        @client.insert(:mod_test, {row: 1, id: 'idf', mod_key: 2, val: '6'})
+
         paging_pipe = CassModKeyEnumerator.new(:mod_test, @client, mod_size = 4, page_size = 1)
         result = paging_pipe.pipe(row: 1).to_a
-        assert_equal 3, result.size
-        assert_equal %w(1 2 3).to_set, result.map { |i| i['val'] }.to_set
+        assert_equal 6, result.size
+        assert_equal %w(1 2 3 4 5 6).to_set, result.map { |i| i['val'] }.to_set
       end
 
       should 'not return items with a mod key outside the mod key range' do
