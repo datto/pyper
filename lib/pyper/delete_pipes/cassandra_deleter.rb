155df2a65248bc291cfe265126c238c660e28a69
Kenneth Hoffmann <kenneth@backupify.com>
2015-11-04 14:49:01 -0500

    Tweaks.

--- lib/pyper/delete_pipes/cassandra_deleter.rb
+++ lib/pyper/delete_pipes/cassandra_deleter.rb
@@ -1,7 +1,7 @@
 module Pyper::DeletePipes
   # Deletes from a specified cassandra table.
 
-  # @param table_name [Symbol] The table in which to store the attributes
+  # @param table_name [Symbol] The table from which to delete
   # @param client [Cassava::Client] client to query cassandra with
   class CassandraDeleter < Struct.new(:table_name, :client)
     # @param args [Hash] Should contain the primary keys to delete. Can contain a :columns key to remove specific values.
@@ -9,10 +9,12 @@ module Pyper::DeletePipes
     # @return [Hash] The original attributes
     # @return [Cassandra::Result] the Cass result
   def pipe(arguments, status = {})
-      columns = arguments.delete(:columns)
+      local_args = arguments.dup
+      columns = local_args.delete(:columns)
 
       statement = columns.present? ? client.delete(table_name, columns) : client.delete(table_name)
-      result = statement.where(arguments).execute
+      statement.where(local_args).execute
+      arguments
     end
   end
 end
